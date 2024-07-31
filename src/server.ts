9cb769ab3abcfbdddf2d31c287de032191669d33
BryanGallo <bryangallo95@hotmail.com>
2024-07-30 23:43:54 -0500

    Conexion a la BDD
    server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
 import express from "express";
 import router from "./routes/router";
+import db from "./config/db";
+
+async function connectDB() {
+    try {
+        await db.authenticate();
+        db.sync();
+        console.log("Conexion a la base de datos exitosa");
+    } catch (error) {
+        console.log(error);
+        console.log('Error al conectar a la bdd');
+    }
+}
+
+connectDB()
 
 const server = express();
 
