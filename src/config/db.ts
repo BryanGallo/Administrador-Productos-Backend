61cb3551fc62f8fcde3f9d344278a27c0e8c41a7
BryanGallo <bryangallo95@hotmail.com>
2024-07-30 23:43:07 -0500

    Configuracion BDD con render a traves de la URL de render
    db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,4 +1,15 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize();
-console.log(db);
+const db = new Sequelize(
+    "postgresql://rest_api_node_typescript_bxyg_user:3dw609JR25LgYJxQBVvcw7wnvLtBSbKC@dpg-cqkrslogph6c738k9bg0-a.oregon-postgres.render.com/rest_api_node_typescript_bxyg?ssl=true",
+    //segunda opcion para el error de conexion de bbd
+    // {
+    //     dialectOptions: {
+    //         ssl: {
+    //             require: false,
+    //         },
+    //     },
+    // }
+);
+
+export default db;
