649fbc25bde960c8338644a374286d402dccff39
BryanGallo <bryangallo95@hotmail.com>
2024-08-01 22:40:44 -0500

    Agregando await al guarda el producto en la base
    Para evitar error con la asyncronia productController.ts

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -4,14 +4,17 @@ import colors from "colors";
 
 const createProduct = async (req: Request, res: Response) => {
     console.log(req.body);
-    
+
     const { name } = req.body;
 
+    //*Forma 1 de insertar usando new
     const product = new Product(req.body);
     console.log(colors.magenta(`${product}`));
-    product.save();
-
-    res.status(200).json({ msg: `Se creo el producto ${name} exitosamente` });
+    const savedProduct = await product.save();
+    res.status(200).json({
+        msg: `Se creo el producto ${name} exitosamente`,
+        data: savedProduct,
+    });
 };
 
 export { createProduct };
