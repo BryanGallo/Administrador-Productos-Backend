71761d7bb40376c08b3c1642ab57117872c64f6b
BryanGallo <bryangallo95@hotmail.com>
2024-08-01 23:01:52 -0500

    Validacion con express-validator
    productController.ts

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { check, validationResult } from "express-validator";
 import Product from "../models/Product.model";
 import colors from "colors";
 
@@ -6,6 +7,37 @@ const createProduct = async (req: Request, res: Response) => {
     console.log(req.body);
 
     const { name } = req.body;
+
+    //Validacion
+    await check("name")
+        .notEmpty()
+        .withMessage("El nombre del producto no puede ir vacio")
+        .run(req);
+
+    await check("price")
+        .isNumeric()
+        .withMessage("Valor no válido")
+        //*reglas personalizadas
+        // .custom((value) => {
+        //     //value es el valor que ingresamos
+        //     if (value <= 0) {
+        //         return false;
+        //     }
+        // })
+        // .withMessage("El precio no puede ser menor a 0")
+        //*con return implicito para reducir codigo
+        .custom((value) => value <= 0)
+        .withMessage("El precio no puede ser menor a 0")
+        .notEmpty()
+        .withMessage("El precio del producto no puede ir vacio")
+        .run(req);
+
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array(),
+        });
+    }
 
     //*Forma 1 de insertar usando new
     // const product = new Product(req.body);
