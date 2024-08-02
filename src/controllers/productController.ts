e367bd78a36daed8d0db510b7649da4232301c70
BryanGallo <bryangallo95@hotmail.com>
2024-08-01 23:32:58 -0500

    Implementando trycatch
    * TryCatch en productController.ts para verificar errores al crear

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -49,13 +49,16 @@ const createProduct = async (req: Request, res: Response) => {
     // });
 
     //*Forma 2 de insertar usando create
-    const product = await Product.create(req.body);
-    console.log(colors.magenta(`${product}`));
+    try {
+        const product = await Product.create(req.body);
 
-    res.status(200).json({
-        msg: `Se creo el producto ${name} exitosamente`,
-        data: product,
-    });
+        res.status(200).json({
+            msg: `Se creo el producto ${name} exitosamente`,
+            data: product,
+        });
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export { createProduct };
