5ad694eeb82d8ddc941fb787676d535f8823b912
BryanGallo <bryangallo95@hotmail.com>
2024-08-01 22:31:39 -0500

    Fn CreateProduct - requiere la configuracion de tsconfig.json
    productController.ts

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from "express";
+import Product from "../models/Product.model";
+import colors from "colors";
 
 const createProduct = async (req: Request, res: Response) => {
     console.log(req.body);
+    
+    const { name } = req.body;
 
-    res.status(200).json(req.body);
+    const product = new Product(req.body);
+    console.log(colors.magenta(`${product}`));
+    product.save();
+
+    res.status(200).json({ msg: `Se creo el producto ${name} exitosamente` });
 };
 
 export { createProduct };
