557a4488fa3fedaba25a1174d931712cdb251a08
BryanGallo <bryangallo95@hotmail.com>
2024-08-06 23:56:15 -0500

    Pruebas PUT por ID para actualizar un producto en productController.ts
    * VALIDACION DE API
    * Verificacion en base a validaciones

--- src/controllers/__test__/productController.test.ts
+++ src/controllers/__test__/productController.test.ts
@@ -79,26 +79,124 @@ describe("GET /api/products", () => {
 describe("GET /api/products/:id", () => {
     it("should return a 404 response for a non-existent product", async () => {
         const productId = 99;
-        const response = await request(server).get(`/api/products/${productId}`);
+        const response = await request(server).get(
+            `/api/products/${productId}`
+        );
         expect(response.status).toBe(404);
         expect(response.body).toHaveProperty("error");
-        expect(response.body.error).toBe(`No existe un producto con el id ${productId}`);
+        expect(response.body.error).toBe(
+            `No existe un producto con el id ${productId}`
+        );
     });
 
-    it("shold check a valid ID in the URL",async ()=>{
+    it("shold check a valid ID in the URL", async () => {
         const productId = "abc";
-        const response = await request(server).get(`/api/products/${productId}`);
+        const response = await request(server).get(
+            `/api/products/${productId}`
+        );
         expect(response.status).toBe(400);
-        expect(response.body).toHaveProperty("errors")
-        expect(response.body.errors).toHaveLength(1)
-        expect(response.body.errors[0].msg).toBe('ID no válido')
-    })
+        expect(response.body).toHaveProperty("errors");
+        expect(response.body.errors).toHaveLength(1);
+        expect(response.body.errors[0].msg).toBe("ID no válido");
+    });
 
-    it("get a JSON response for a single product",async ()=>{
+    it("get a JSON response for a single product", async () => {
         const productId = 1;
-        const response = await request(server).get(`/api/products/${productId}`);
+        const response = await request(server).get(
+            `/api/products/${productId}`
+        );
         expect(response.status).toBe(200);
-        expect(response.body).toHaveProperty("data")
-        expect(response.body.data).toHaveLength(4)
-    })
+        expect(response.body).toHaveProperty("data");
+        expect(response.body.data).toBeTruthy();
+    });
+});
+
+describe("PUT /api/products/:id", () => {
+    it("shold check a valid ID in the URL", async () => {
+        const productId = "abc";
+        const response = await request(server)
+            .put(`/api/products/${productId}`)
+            .send({
+                name: "Monitor SAMSUNG 24 pulgadas ",
+                price: 600,
+                description: "Monitor S Gaming 120HZ 1ms ",
+                availability: true,
+            });
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty("errors");
+        expect(response.body.errors).toHaveLength(1);
+        expect(response.body.errors[0].msg).toBe("ID no válido");
+    });
+
+    it("should display validation error messages when updating a product", async () => {
+        const productId = 1;
+        const response = await request(server)
+            .put(`/api/products/${productId}`)
+            .send({});
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty("errors");
+        expect(response.body.errors).toBeTruthy();
+        expect(response.body.errors).toHaveLength(5);
+
+        expect(response.status).not.toBe(200);
+        expect(response.body).not.toHaveProperty("data");
+    });
+
+    it("should validate that the price is greater than 0", async () => {
+        const productId = 1;
+        const response = await request(server)
+            .put(`/api/products/${productId}`)
+            .send({
+                name: "Monitor SAMSUNG 24 pulgadas ",
+                price: -600,
+                description: "Monitor S Gaming 120HZ 1ms ",
+                availability: true,
+            });
+        expect(response.status).toBe(400);
+        expect(response.body).toHaveProperty("errors");
+        expect(response.body.errors).toBeTruthy();
+        expect(response.body.errors).toHaveLength(1);
+        expect(response.body.errors[0].msg).toBe(
+            "El precio no puede ser menor o igual a 0"
+        );
+
+        expect(response.status).not.toBe(200);
+        expect(response.body).not.toHaveProperty("data");
+    });
+
+    it("should return a 404 response for a non-existent product", async () => {
+        const productId = 200;
+        const response = await request(server)
+            .put(`/api/products/${productId}`)
+            .send({
+                name: "Monitor SAMSUNG 24 pulgadas ",
+                price: 600,
+                description: "Monitor S Gaming 120HZ 1ms ",
+                availability: true,
+            });
+        expect(response.status).toBe(404);
+        expect(response.body).toHaveProperty("error");
+        expect(response.body.error).toBe("No existe un producto con el id 200");
+        
+        expect(response.status).not.toBe(200);
+        expect(response.body).not.toHaveProperty("data");
+    });
+
+    it("should update an existing product with valid data", async () => {
+        const productId = 1;
+        const response = await request(server)
+            .put(`/api/products/${productId}`)
+            .send({
+                name: "Monitor SAMSUNG 24 pulgadas ",
+                price: 600,
+                description: "Monitor S Gaming 120HZ 1ms ",
+                availability: true,
+            });
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveProperty("data");
+        expect(response.body.data).toBeTruthy();
+
+        expect(response.status).not.toBe(404);
+        expect(response.body).not.toHaveProperty("errors");
+    });
 });
