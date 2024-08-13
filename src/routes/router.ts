2acca8120bc8f5a89418ee03c4a300e25e03f3f6
BryanGallo <bryangallo95@hotmail.com>
2024-08-13 02:06:50 -0500

    Post de /api/products
    * Generando documentacion para crear un producto
    * Usando requestBody para indicar que es obligatorio el cuerpo de la solicitud
    * Ejemplo del requestBody
    * Indicando los diferentes responses 200-400

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -121,7 +121,59 @@ router.get(
     getProductById
 );
 
-
+//*Recuerda en schema(linea 136) hay 2 opciones usar  $ref: '#/components/schemas/Product'
+//*Lo que tomara la base del Schema creado al inicio para dar un ejemplo de properties(linea 158)
+//* o como en este caso que no se usa y se indica las propiedades a enviar manualmente(linea 140 - 152) 
+/**
+ * @swagger
+ * /api/products:
+ *   post:
+ *     summary: Create a new product
+ *     tags: [Products]
+ *     description: Returns a new record in database
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                  type: string
+ *                  description: The name of the product
+ *                  example: "Monitor Curvo LG 49 pulgadas"
+ *               price:
+ *                  type: number
+ *                  description: The price of the product
+ *                  example: 300
+ *               description:
+ *                  type: string
+ *                  description: Description of the product
+ *                  example: "Monitor Gaming 120HZ 1ms"
+ *     responses:
+ *       201:
+ *         description: Successful response
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Product'
+ *       400:
+ *         description: Invalid input data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 errors:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       msg:
+ *                         type: string
+ *                         oneOf:
+ *                           - example: "El precio debe ser mayor a 0 o El nombre del producto no puede ir vacio "
+ */
 
 //Validacion con express-validator en el router
 //Router no es asyncrono asi que no usamos check usamos body
